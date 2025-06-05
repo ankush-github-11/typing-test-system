21cdef43c882e694ccca8e20df24e4f055ee26a4
ankush-github-11 <ankush10yt@gmail.com>
2025-06-05 21:40:51 +0530

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+
+import './App.css'
+function App() {
+  return (
+    <>
+    <div className='h-screen w-screen text-2xl text-cyan-500 bg-[#121212] flex justify-center items-center font-bold tracking-[1px]'>Hello Typists!</div>
+    </>
+  )
+}
+export default App
