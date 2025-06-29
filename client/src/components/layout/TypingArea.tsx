0bf56dc057797d2c51123f48f99f5c8b09e02264
ankush-github-11 <ankush10yt@gmail.com>
2025-06-30 00:07:03 +0530

    Fixing Bugs in Typing Area

--- client/src/components/layout/TypingArea.tsx
+++ client/src/components/layout/TypingArea.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react"
-import Cursor from "../ui/Cursor"
+// import Cursor from "../ui/Cursor"
 const TypingArea = () => {
     const [targetText] = useState("A developer is someone who solves problems using logic and code while continuously learning and adapting to new technologies in order to build efficient and innovative solutions");
     const [typedText, setTypedText] = useState("");
     return (
         <div className='select-none ml-5 mt-25 text-4xl pt-[30px] pb-[30px] min-h-[35vh] h-fit w-[85%] font-mono text-gray'>
-            <input className="min-h-[35vh] w-[85%] absolute opacity-0 cursor-default" type="text" value={typedText} onChange={(e) => setTypedText(e.target.value)} autoFocus />
+            <label htmlFor="typing-input" className="hidden">Typing Input</label>
+            <input id="typing-input" className="min-h-[35vh] w-[85%] absolute opacity-0 cursor-default" type="text" value={typedText} onChange={(e) => setTypedText(e.target.value)} autoFocus />
             {targetText.split("").map((char, i) => {
                 const typedChar = typedText[i];
                 let className = "text-gray-400";
