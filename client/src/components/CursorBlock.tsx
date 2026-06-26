541b8e6ae7dbd87183e13a44d1714dda21d16a55
ankush-github-11 <ankush10yt@gmail.com>
2026-06-26 13:57:00 +0530

    Adding cursor smoothness

--- client/src/components/CursorBlock.tsx
+++ client/src/components/CursorBlock.tsx
@@ -1,10 +1,17 @@
 import "../styles/cursor.css";
 import { useTypingAreaFocusedStore } from "../store/useTypingAreaFocusedStore";
 import { useTestStartedStore } from "../store/useTestStartedStore";
+import { useSettingsStore } from "../store/useSettingsStore";
 
 const CursorBlock = ({ top, left, cn }: { top: number; left: number; cn: string }) => {
   const focused = useTypingAreaFocusedStore((state) => state.focused);
   const started = useTestStartedStore((state) => state.testStarted);
+  const cursorSmoothness = useSettingsStore((state) => state.cursorSmoothness);
+  let durationClass = "";
+  if(cursorSmoothness === "off") durationClass = "duration-0";
+  if(cursorSmoothness === "low") durationClass = "duration-90";
+  if(cursorSmoothness === "medium") durationClass = "duration-140";
+  if(cursorSmoothness === "high") durationClass = "duration-190";
   return (
     <>
       <div
@@ -12,7 +19,7 @@ const CursorBlock = ({ top, left, cn }: { top: number; left: number; cn: string 
           top: `${top}px`,
           left: `${left}px`,
         }}
-        className={`${started || focused ? "" : "blur-md"} transition-all duration-150 ease-linear mt-[1px] h-[45px] w-[23px] border-2 border-color1 inline-block absolute rounded-sm ${cn}`}
+        className={`${started || focused ? "" : "blur-md"} transition-all ${durationClass} ease-linear mt-[1px] h-[45px] w-[23px] border-2 border-color1 inline-block absolute rounded-sm ${cn}`}
       ></div>
     </>
   );
--- client/src/components/CursorDefault.tsx
+++ client/src/components/CursorDefault.tsx
@@ -1,10 +1,17 @@
 import "../styles/cursor.css";
 import { useTypingAreaFocusedStore } from "../store/useTypingAreaFocusedStore";
 import { useTestStartedStore } from "../store/useTestStartedStore";
+import { useSettingsStore } from "../store/useSettingsStore";
 
 const CursorDefault = ({ top, left, cn }: { top: number; left: number; cn: string }) => {
   const focused = useTypingAreaFocusedStore((state) => state.focused);
   const started = useTestStartedStore((state) => state.testStarted);
+  const cursorSmoothness = useSettingsStore((state) => state.cursorSmoothness);
+  let durationClass = "";
+  if(cursorSmoothness === "off") durationClass = "duration-0";
+  if(cursorSmoothness === "low") durationClass = "duration-90";
+  if(cursorSmoothness === "medium") durationClass = "duration-140";
+  if(cursorSmoothness === "high") durationClass = "duration-190";
   return (
     <>
       <div
@@ -12,7 +19,7 @@ const CursorDefault = ({ top, left, cn }: { top: number; left: number; cn: strin
           top: `${top}px`,
           left: `${left}px`,
         }}
-        className={`${started || focused ? "" : "blur-md"} transition-all duration-150 ease-linear h-[45px] w-[2.5px] bg-color1 inline-block absolute rounded-full ${cn}`}
+        className={`${started || focused ? "" : "blur-md"} transition-all ${durationClass} ease-linear h-[45px] w-[2.5px] bg-color1 inline-block absolute rounded-full ${cn}`}
       ></div>
     </>
   );
