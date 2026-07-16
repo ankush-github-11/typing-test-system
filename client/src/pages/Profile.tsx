811c8396d1b32cfe0a6bbe50bb25a06221369d4b
ankush-github-11 <ankush10yt@gmail.com>
2026-07-17 00:29:05 +0530

    Updating profile page logic

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -4,8 +4,18 @@ import { useTheme } from "../context/useTheme";
 import useButtonNavigator from "../hooks/useButtonNavigator";
 import { useMe } from "../hooks/useMe";
 import { useTitle } from "../hooks/useTitle";
-import { LaptopMinimalCheck, Rocket, SquareUser, ClockArrowUp, MapPin, Building2, Pencil, Flag } from 'lucide-react';
+import {
+  LaptopMinimalCheck,
+  Rocket,
+  SquareUser,
+  ClockArrowUp,
+  MapPin,
+  Building2,
+  Pencil,
+  Flag,
+} from "lucide-react";
 import { Link } from "react-router-dom";
+import { UserRoundPen } from "lucide-react";
 
 const Profile = () => {
   const { isDark } = useTheme();
@@ -39,79 +49,137 @@ const Profile = () => {
           {/*Left Div*/}
           <div className="flex-[2.5] min-h-screen h-fit rounded-xl pt-5">
             <div className="h-fit w-fit flex gap-x-3 items-center">
-              <SquareUser size={120} strokeWidth={1}/>
+              <SquareUser size={120} strokeWidth={1} />
               <div className="flex flex-col gap-y-1 w-full h-fit">
                 <p className="text-[16px]">{user.name}</p>
                 <p className="text-[14px] text-textcolorless">Blank Username</p>
-                <p className="text-[14px]">Rank <span>Blank Rank</span></p>
+                <p className="text-[14px]">
+                  Rank <span>Blank Rank</span>
+                </p>
               </div>
             </div>
             <div className="h-fit w-full flex items-center gap-x-3 mb-5">
-              <p className="w-fit text-color1 font-semibold text-[16px]">{user.level}</p>
+              <p className="w-fit text-color1 font-semibold text-[16px]">
+                {user.level}
+              </p>
               <div className="w-full h-2 bg-lightgray rounded-full">
                 <div className="w-[80%] h-2 bg-gray rounded-full"></div>
               </div>
-              <p className="w-fit text-textcolorless"><span className="font-semibold">{user.xp}</span>/2300</p>
+              <p className="w-fit text-textcolorless">
+                <span className="font-semibold">{user.xp}</span>/2300
+              </p>
             </div>
             <div className="h-fit w-full mb-5">
               <p className="text-[15px]">{user.bio}</p>
             </div>
-            <div className="h-fit w-full mb-1 flex gap-x-3 items-center">
-              <p className="text-[16px] font-semibold text-textcolorless/50">Keyboard</p>
-              <p className="text-[15px]">{user.keyboard}</p>
-            </div>
+            {user.keyboard && (
+              <div className="h-fit w-full mb-1 flex gap-x-3 items-center">
+                <p className="text-[16px] font-semibold text-textcolorless/50">
+                  Keyboard
+                </p>
+                <p className="text-[15px]">{user.keyboard}</p>
+              </div>
+            )}
             <div className="h-fit w-full mb-5 flex gap-x-3 items-center">
-              <p className="text-[16px] font-semibold text-textcolorless/50">Date Joined</p>
+              <p className="text-[16px] font-semibold text-textcolorless/50">
+                Date Joined
+              </p>
               <p className="text-[15px]">Blank Date Joined</p>
             </div>
             <div className="mb-5 w-full h-fit select-none">
-              <Link to="/edit" className="rounded-md text-[16px] border-2 border-color1 h-10 w-full flex justify-center items-center">
+              <Link
+                to="/edit"
+                className="rounded-md text-[16px] border-2 border-color1 h-10 w-full flex justify-center items-center gap-x-1"
+              >
+                <UserRoundPen className="text-color1" size={17} />
                 <span className="text-color1 text-[17px]">Edit Profile</span>
               </Link>
             </div>
             <div className="h-fit w-full flex flex-col gap-y-[8px] mb-5">
               <p className="text-[17px] font-semibold mb-1">Overall Stats</p>
               <div className="w-fit flex items-center">
-                <Rocket size={18} strokeWidth={2} className="text-pink-500 mr-2"/>
-                <p className="text-[14.5px] text-textcolorless/70 mr-5">Test Started</p>
+                <Rocket
+                  size={18}
+                  strokeWidth={2}
+                  className="text-pink-500 mr-2"
+                />
+                <p className="text-[14.5px] text-textcolorless/70 mr-5">
+                  Test Started
+                </p>
                 <p className="text-[16px]">{user.test_started}</p>
               </div>
               <div className="w-fit flex items-center">
-                <LaptopMinimalCheck size={18} strokeWidth={2} className="text-emerald-500 mr-2"/>
-                <p className="text-[14.5px] text-textcolorless/70 mr-5">Test Completed</p>
+                <LaptopMinimalCheck
+                  size={18}
+                  strokeWidth={2}
+                  className="text-emerald-500 mr-2"
+                />
+                <p className="text-[14.5px] text-textcolorless/70 mr-5">
+                  Test Completed
+                </p>
                 <p className="text-[16px]">{user.test_completed}</p>
               </div>
               <div className="w-fit flex items-center">
-                <ClockArrowUp size={18} strokeWidth={2} className="text-yellow-500 mr-2" />
-                <p className="text-[14.5px] text-textcolorless/70 mr-5">Time Typing</p>
+                <ClockArrowUp
+                  size={18}
+                  strokeWidth={2}
+                  className="text-yellow-500 mr-2"
+                />
+                <p className="text-[14.5px] text-textcolorless/70 mr-5">
+                  Time Typing
+                </p>
                 <p className="text-[16px]">{formatTime(user.time_typing)}</p>
               </div>
               <div className="w-fit flex items-center">
-                <Pencil size={18} strokeWidth={2} className="text-blue-500 mr-2" />
-                <p className="text-[14.5px] text-textcolorless/70 mr-5">Total Chars Typed</p>
+                <Pencil
+                  size={18}
+                  strokeWidth={2}
+                  className="text-blue-500 mr-2"
+                />
+                <p className="text-[14.5px] text-textcolorless/70 mr-5">
+                  Total Chars Typed
+                </p>
                 <p className="text-[16px]">{user.total_chars_typed}</p>
               </div>
               <div className="w-fit flex items-center">
-                <Flag size={18} strokeWidth={2} className="text-orange-500 mr-2" />
-                <p className="text-[14.5px] text-textcolorless/70 mr-5">Longest Streak</p>
+                <Flag
+                  size={18}
+                  strokeWidth={2}
+                  className="text-orange-500 mr-2"
+                />
+                <p className="text-[14.5px] text-textcolorless/70 mr-5">
+                  Longest Streak
+                </p>
                 <p className="text-[16px]">{user.longest_streak}</p>
               </div>
             </div>
-            <div className="h-fit w-full flex flex-col gap-y-[8px]">
-              <p className="text-[17px] font-semibold mb-1">Details</p>
-              <div className="w-fit flex items-center">
-                <MapPin size={18} strokeWidth={2} className="text-textcolorless/60 mr-2"/>
-                <p className="text-[14.5px] mr-5 text-textcolorless">
-                  <span>{user.city}</span>
-                  <span>,{" "}</span>
-                  <span>{user.country}</span>
-                </p>
+            {user.city && user.country && user.organization && (
+              <div className="h-fit w-full flex flex-col gap-y-[8px]">
+                <p className="text-[17px] font-semibold mb-1">Details</p>
+                <div className="w-fit flex items-center">
+                  <MapPin
+                    size={18}
+                    strokeWidth={2}
+                    className="text-textcolorless/60 mr-2"
+                  />
+                  <p className="text-[14.5px] mr-5 text-textcolorless">
+                    <span>{user.city}</span>
+                    <span>, </span>
+                    <span>{user.country}</span>
+                  </p>
+                </div>
+                <div className="w-fit flex items-center">
+                  <Building2
+                    size={18}
+                    strokeWidth={2}
+                    className="text-textcolorless/60 mr-2"
+                  />
+                  <p className="text-[14.5px] mr-5 text-textcolorless">
+                    {user.organization}
+                  </p>
+                </div>
               </div>
-              <div className="w-fit flex items-center">
-                <Building2 size={18} strokeWidth={2} className="text-textcolorless/60 mr-2"/>
-                <p className="text-[14.5px] mr-5 text-textcolorless">{user.organization}</p>
-              </div>
-            </div>
+            )}
           </div>
           {/*Right Div*/}
           <div className="flex-[7.5] min-h-screen h-fit bg-bgcolorless rounded-xl"></div>
