94d011ac3a49fd8d42923c775b2418a6a8b143b0
ankush-github-11 <ankush10yt@gmail.com>
2026-08-12 22:06:43 +0530

    Fixing vercel errors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Leaderboard from "./pages/Leaderboard"
-import About from "./pages/About"
 import Features from "./pages/Features"
 import Settings from "./pages/Settings"
 import TypingTest from "./pages/TypingTest"
@@ -17,7 +16,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/leaderboard" element={<Leaderboard />}/>
-        <Route path="/about" element={<About />}/>
         <Route path="/features" element={<Features />}/>
         <Route path="/settings" element={<Settings />}/>
         <Route path="/typingtest" element={<TypingTest />}/>
